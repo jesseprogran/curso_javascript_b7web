76acdd65565d7e675b3393ec50f572947484c082
Jesse Silva <jessepescador22@gmail.com>
2024-01-16 19:21:10 -0300

    aula de tipos de dados

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,50 @@
-//  VARIÁVEIS
+//  VARIÁVEIS 
+// var nome = 'josé'
 
-var nome = 'josé'
+// let idade = 32
 
-let idade = 32
+// const sobreNome = 'silva'
 
-const sobreNome = 'silva'
-
-console.log(nome, sobreNome ,  idade);
-
-
-let estado = 'CE'
-
-let cidade = 'Icapuí'
+// console.log(nome, sobreNome ,  idade);
 
 
-console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro na cidade de ${cidade}, capital é ${estado}..`)
+// let estado = 'CE'
+
+// let cidade = 'Icapuí'
+
+
+// console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro na cidade de ${cidade}, capital é ${estado}..`)
+
+
+//TIPO DE DADOS 
+// string
+let nome = "Jessé";
+// Number
+let idade = 32;
+// true 
+let casado = true;
+// false
+let netos = false;
+// arrey
+let nomeFilhos = ["Manuela", "Isaac"];
+// objeto
+let nomeCompleto = {
+    nome:"Jessé",
+    idade: 32,
+    casado: true,
+    filhos: true,
+    netos: false
+}
+
+// VERIFICANDO O TIPO DE DADOS 
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof casado)
+console.log(typeof nomeFilhos)
+console.log(typeof netos)
+console.log(typeof nomeCompleto)
+
+
+
+
+
