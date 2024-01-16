20eaa6560226d1d13476d7d7402ee37f9e5b7e6d
Jesse Silva <jessepescador22@gmail.com>
2024-01-15 21:56:59 -0300

    variaveis

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
-console.log('Olá Mundo')
+//  VARIÁVEIS
+
+var nome = 'josé'
+
+let idade = 32
+
+const sobreNome = 'silva'
+
+console.log(nome, sobreNome ,  idade);
+
+
+let estado = 'CE'
+
+let cidade = 'Icapuí'
+
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos, moro na cidade de ${cidade}, capital é ${estado}..`)
