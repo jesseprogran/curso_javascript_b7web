36ebee601d14a5d6e79db7223dbeef0d76c6ad3a
Jesse Silva <jessepescador22@gmail.com>
2024-01-17 22:17:22 -0300

    realizando exercicio de if else

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,31 @@
-// CONDICIONAIS 
+// Exercicios de Condicionais
+// 01 - Substitua o @ pela condicional correta para que 
+// o resultado seja TRUE.
+let x = 10;
+let y = 5;
 
-let valorMeuCarro = 50.000;
+console.log(x !== y);
 
-// if (valorMeuCarro === 49.000 ) {
-//     console.log("Valor do Meu carro");
-// }else {
-//     console.log("Esse não foi o valor do meu CARRO!!!");
-// }
+// 02 - Substitua o @ pela condicional correta para que 
+// o resultado seja FALSE.
+let w = "10";
+let z = 10;
 
-// <
-// >
-// ==
-// >=
-// <=
-// !=
+console.log(w === z);
+// 03 -  Substitua o @ pela condicional correta para que 
+// o resultado seja TRUE.
 
+let a = 10;
+let b = 5;
 
+console.log(a != b);
 
-//  MULTICONDICIONAIS
+// 04 - Crie uma condicional para verificar se o preço da 
+// carne está barato ou caro. PS: Até 45 está barato.
+let preco = 10;
 
-let idade = 50;
-
-if (idade >= 18 && idade < 60) {
-    console.log("você é adulto.");
+if (preco <= 45) {
+    console.log("ESTÁ BARATO");
+} else {
+    console.log("ESTÁ CARA");
 }
