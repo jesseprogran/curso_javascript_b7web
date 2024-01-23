f1232fe065d0a5ef7b0269143630a18c98500172
Jesse Silva <jessepescador22@gmail.com>
2024-01-22 22:33:10 -0300

    começando estudo sobre funções

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,8 @@
-//  Condicional Switch
+// FUNÇÕES
+    function gravidade() {
+        console.log(`A gravidade do planeta é: `);
+        console.log(9.8);
+    }
 
-let profession = "Bombeiro";
+    gravidade();
 
-console.log("Profissão: " + profession);
-
-switch (profession) {
-    case 'fiscal':
-        console.log('Sua carreira será VERDE');
-        break;
-    case 'bombeiro':
-        console.log('Sua carreira será VERMELHA');
-        break;
-    case 'policial':
-        console.log('Sua carreira será AZUL');
-        break;
-    default:
-        console.log('Quando for nenhuma será PRETA');
-        break;
-}
