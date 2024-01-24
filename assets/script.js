10c0f1c66d7915e162f853f9ea86075dde0b9b3a
Jesse Silva <jessepescador22@gmail.com>
2024-01-24 00:03:16 -0300

    realizando exercicio 02 sobre função

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 // EXERCICIOS SOBRE FUNÇÃO 
-// 01 - Calcule a porcentagem entre 2 números.
-// Exemplo: 25% de 40 é 10
-// Fórmula da porcentagem: (y / x) * 100
-// Uso da Função:
-
-// let x = 40;
-// let y = 10;
-// let pct = calcPct(x, y);
-//console.log(`${pct}% de ${x} é ${y}`);
-
-
-function calcPct(x, y) {
-    return (y / x) * 100
+/* Calcule o preço do imovel
+    - m2 = 3.000
+    - Se tiver 1 quarto, o m2 é 1x
+    - Se tiver 2 quarto, o m2 é 1.2x
+    - Se tiver 3 quartos, o m2 é 1.5x
+    Uso da Função:
+*/
+function calcularImovel(metragem, quartos ) {
+    let m2 = 3000
+    if (quartos === 1) {
+        return metragem * m2;
+    } else if (quartos === 2) {
+        return metragem * ( m2 * 1.2);
+    } else if (quartos === 3) {
+        return metragem * ( m2 * 1.5);
+    } else {
+        return m2;
+    }
 }
 
-let x = 40;
-let y = 10;
+let metragem = 123;
+let quartos = 1;
+let preco = calcularImovel(metragem, quartos);
+console.log(` A sua casa custa R$ ${preco}`);
 
-let pct = calcPct(x, y);
-console.log(`${pct}% de ${x} é ${y}`);
+
