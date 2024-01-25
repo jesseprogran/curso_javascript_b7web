bbe464bbaf0be627c0fbcca5d974284d2613c765
Jesse Silva <jessepescador22@gmail.com>
2024-01-25 20:40:39 -0300

    Iniciando aula sobre objetos

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,21 @@
-// Exercicios Array;
+// Objetos;
 
-// 01- No array abaixo, qual o número que pega a Ferrari?
+let personagem = {
+    nome: 'Batman',
+    idade: 50,
+    origem: 'Terra',
+    uniformes: ['preto', 'azul', 'verde'],
+    poderes: {
+        forca: 87,
+        habilidade: 75,
+        magia: 0,
+        velocidade: 56
+    }
+};
 
-let carros = [
-    'BMW',
-    'Ferrari',
-    'Mercedes'
-];
-
-let x = 1
-console.log('1. ' + carros[x]);
-
-// 02- Troque a Ferrari Por Audi;
-carros[1] = 'Audi';
-console.log('2. Lista com Audi: ');
-console.log(carros);
-
-// 03- Adcione o Volvo a lista;
-carros.push('Volvo');
-console.log('3. Lista com Volvo: ');
-console.log(carros);
-
-// 04- Exiba quantos itens tem no array;
-console.log('4. itens no array: ' + carros.length);
+console.log(`Esse personagem ${personagem.nome} tem  como habilidade ${personagem.poderes.habilidade} %.`)
+console.log(`O personagem ${personagem.nome} tem o uniforme da cor ${personagem.uniformes[2]}.`);
+console.log(`Esse personagem tem o nome de ${personagem.nome} e tem a idade de ${personagem.idade} anos.`);
+console.log(personagem);
 
 
