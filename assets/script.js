8f5998a066e137427420f3dd3732d181bb30b466
Jesse Silva <jessepescador22@gmail.com>
2024-01-26 19:57:27 -0300

    funções dentro do objeto

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,13 @@
-// Acessando e alterando Objetos
+// funções dentro de Objetos;
 
-let personagem = {
-    nome: 'Batman',
-    idade: 50,
-    origem: 'Terra',
-    uniformes: ['preto', 'azul', 'verde'],
-    poderes: {
-        forca: 87,
-        habilidade: 75,
-        magia: 0,
-        velocidade: 56
+let pessoa = {
+    nome: 'Manuela',
+    sobrenome: 'Valente',
+    idade: 7,
+    nomeCompleto: function () {
+        return `${this.nome} ${this.sobrenome}`;
     }
-};
-
-personagem.nome = 'Super Man';
-personagem.poderes.forca += 10;
-personagem.uniformes.pop();
-
-console.log(personagem.nome);
-
-const car = {
-    nome: 'Jessé',
-    idade: 32,
-    carros: [
-        {modelo: 'GOL', cor: 'PRETO'},
-        {modelo: 'FIAT', cor: 'BRANCO'}
-    ]
 }
-car.carros[0].modelo;
 
-console.log(car.carros);
+
+console.log(pessoa.nomeCompleto());
