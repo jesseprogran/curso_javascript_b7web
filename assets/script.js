b576406ce815d6cc9f16a356142ff53258723ba5
Jesse Silva <jessepescador22@gmail.com>
2024-01-31 18:19:27 -0300

    resolvendo exercicio de loop

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,16 @@
-// loop for em arrays
+// EXERCICIO DE LOOP
 
-let melhoresCorredores = ['Lorin', 'Luiz Carlos', 'João', 'Juciê'];
-melhoresCorredores.push('Jessé');
-melhoresCorredores.push('Davi');
+// Faça um loop que mostre todas as frutas
 
-for( i = 0; i < melhoresCorredores.length; i++ ){
-    console.log('Esses foram os Melhores ' + melhoresCorredores[i]);
-}
+fruits = ['Maça', 'Uva', 'Banana'];
+ for (let x in fruits) {
+    console.log(fruits[x]);
+ }
 
-//exemplo pegando a chave do array
-
-for ( z in melhoresCorredores) {
-    console.log(melhoresCorredores[z]);
-}
-
-// pegando pelo valor
-
-for (jogador of melhoresCorredores) {
-    console.log(jogador);
-}
-
-//////////
-
-
-let cores = [
-    {nome:'Vermelho', qtd: 23},
-    {nome: 'Preto', qtd: 15},
-    {nome:'Azul', qtd: 29}
-]
-
-for (i = 0; i < cores.length; i++) {
-console.log(`Cores Disponiveis: ${cores[i].nome} ${cores[i].qtd}`);
-}
-
-for (v in cores) {
-    console.log(`Cores e Quantidades: ${cores[v].nome} ${cores[v].qtd}`);
-}
-
-for (valor of cores) {
-    console.log(`C e Q : ${valor.nome} ${valor.qtd}`);
-}
+ // conte de 1 até 100 através de um wile
+ let num = 1;
+ let num2 = 100;
+ while(num <= num2) {
+   console.log(num);
+   num++;
+ }
