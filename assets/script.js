41173772c20e34f46abb59d6925de9bb4393848c
Jesse Silva <jessepescador22@gmail.com>
2024-02-01 19:23:14 -0300

    aula sobre filter, some, includes e every

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
-// Funções de array
+// Métodos de Interação
+
+let frutas = ['Banana', 'Maçã', 'Goiaba', 'uva'];
+
+// let bigFrutas = frutas.filter((value) => value.length > 4);
+
+// let check = frutas.every((value) => value.length > 3);
+// let check = frutas.some((value) => value.length > 3);
 
 
-let carro = ['Davi', 'Jessé', 'Dedé'];
+// if (check) {
+//    console.log(`Todos são maior do que 3`);
+// } else {
+//    console.log(`Não são todos maior que 3`);
+// }
 
-carro[carro.length - 1] = 'Papai';
+// Método includes ele verifica se tem o intem dentro do  array.
+if (frutas.includes('Melão')) {
+   console.log('Tem sim!');
+} else {
+   console.log('Não tem..');
+}
 
-console.log(carro)
