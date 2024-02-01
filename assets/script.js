4fd9319326714532c6aed7358182c9660e693085
Jesse Silva <jessepescador22@gmail.com>
2024-02-01 20:38:12 -0300

    aula sobre eventos

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,9 @@
-// Métodos de Interação
+// Eventos de Click;
 
-let frutas = ['Banana', 'Maçã', 'Goiaba', 'uva'];
-
-// let bigFrutas = frutas.filter((value) => value.length > 4);
-
-// let check = frutas.every((value) => value.length > 3);
-// let check = frutas.some((value) => value.length > 3);
-
-
-// if (check) {
-//    console.log(`Todos são maior do que 3`);
-// } else {
-//    console.log(`Não são todos maior que 3`);
-// }
-
-// Método includes ele verifica se tem o intem dentro do  array.
-if (frutas.includes('Melão')) {
-   console.log('Tem sim!');
-} else {
-   console.log('Não tem..');
+function clicou() {
+   console.log('Deu Certo...')
 }
-
+let botao = document.querySelector('.botao');
+botao.addEventListener('click', () => {
+   clicou();
+});
