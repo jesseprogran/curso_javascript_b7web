f89170644fdf1c24e34c23f3d732a0b8e8c0b0ce
Jesse Silva <jessepescador22@gmail.com>
2024-02-02 20:38:27 -0300

    aula sobre os metodo before e after

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,14 @@ function clicou() {
   const teste = document.querySelector('#teste');
   const ul = teste.querySelector('ul');
 
-  let newLi = document.createElement('li');
-  newLi.innerText = 'Tu é fera de Mais....';
+  let newUl = document.createElement('ul');
+//   ul.after(newUl); // os elemenos vao pra ultimo ;
+  ul.before(newUl); // os elementos vai pra cima 
 
-  ul.appendChild(newLi);// adciona ao final ;
-  ul.prepend(newLi); // adciona o elemento no começo;
+  for (let i = 0; i < 5; i++) {
+      let newLi = document.createElement('li');
+      newLi.innerText = `Item add ${i + 1}`;
+      newUl.append(newLi);
+  }
    
 }
