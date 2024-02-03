3fa522edab1642e4001547de26aa33dade9865e6
Jesse Silva <jessepescador22@gmail.com>
2024-02-02 21:33:05 -0300

    manipulando atributos

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
 // Eventos de Click;
 
 function clicou() {
-  const teste = document.querySelector('#teste');
-  const ul = teste.querySelector('ul');
+   const input = document.querySelector('input');
+   const botao = document.querySelector('.botao');
 
-  let newUl = document.createElement('ul');
-//   ul.after(newUl); // os elemenos vao pra ultimo ;
-  ul.before(newUl); // os elementos vai pra cima 
-
-  for (let i = 0; i < 5; i++) {
-      let newLi = document.createElement('li');
-      newLi.innerText = `Item add ${i + 1}`;
-      newUl.append(newLi);
-  }
+   if (input.getAttribute('type') === 'text') {
+      input.setAttribute('type', 'password');
+      botao.innerText = `Mostrar Senha`;
+   } else {
+      input.setAttribute('type', 'text');
+      botao.innerText = `Ocultar Senha`;
+   }
    
 }
