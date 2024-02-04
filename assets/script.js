2188f3a7e9de28ae68141d3dd0627b6e6d2d6dbc
Jesse Silva <jessepescador22@gmail.com>
2024-02-04 20:46:10 -0300

    aula sobre Orientação a Objetos

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,21 @@
-// Elementos
-   const input = document.querySelector('input');
-   const lista = document.querySelector('ul');
-// Funções
-function handleKeyUp(event) {
-   if (event.key === 'Enter') {
-      // Adicionar o elemento LI na lista
-      const newLi = document.createElement('li');
-      newLi.innerHTML = input.value;
-      lista.appendChild(newLi);
+// Orientação a Objetos
+class Person {
 
-      // Limpar o campo de texto
-      input.value = '';
+   age = 0;
+   constructor(name) {
+      this.name = name;
    }
 }
-// Eventos
-input.addEventListener('keyup', handleKeyUp);
+
+// Instância
+let p1 = new Person('Jessé');
+let p2 = new Person('Dalila');
+let p3 = new Person('Davi');
+
+p1.age = 33;
+p2.age = 37;
+p3.age = 38;
+
+console.log(`P1 = ${p1.name} tem ${p1.age}`);
+console.log(`P2 = ${p2.name} tem ${p2.age}`);
+console.log(`P3 = ${p3.name} tem ${p3.age}`);
