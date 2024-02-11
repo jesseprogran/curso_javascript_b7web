aa64cb9364ac6e7ee585e2a01ca67f3a006e97bf
Jesse Silva <jessepescador22@gmail.com>
2024-02-10 21:03:46 -0300

    criando ataques

--- assets/classes.js
+++ assets/classes.js
@@ -83,22 +83,38 @@ class Stage {
 
   update() {
     // fighter 1
-    this.fighter1El.querySelector(
-      ".name"
-    ).innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+    this.fighter1El.querySelector(".name").innerHTML = `${
+      this.fighter1.name
+    } - ${this.fighter1.life.toFixed(1)} HP`;
 
     let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
     this.fighter1El.querySelector(".bar").style.width = `${f1Pct}%`;
     // fighter 2
-    this.fighter2El.querySelector(
-      ".name"
-    ).innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+    this.fighter2El.querySelector(".name").innerHTML = `${
+      this.fighter2.name
+    } - ${this.fighter2.life.toFixed(1)} HP`;
 
     let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
     this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
   }
 
   doAttack(attracking, attacked) {
-    console.log(`${attracking.name} está atacando ${attacked.name}`);
+    if (attracking.life <= 0 || attacked.life <= 0) {
+      console.log(`Atacando Cachorro Morto`);
+      return;
+    }
+
+    let attackFactor = (Math.random() * 2).toFixed(2);
+    let defenseFactor = (Math.random() * 2).toFixed(2);
+
+    let actualAttack = attracking.attack * attackFactor;
+    let actualDefense = attacked.defense * defenseFactor;
+
+    if (actualAttack > actualDefense) {
+      attacked.life -= actualAttack;
+    } else {
+      console.log(`${attacked.name} conseguiu defender....`);
+    }
+    this.update();
   }
 }
