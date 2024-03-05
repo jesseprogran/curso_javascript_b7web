b4ab76ab34e69f05877127f6d7a976e583cd6465
Jesse Silva <jessepescador22@gmail.com>
2024-03-05 19:55:18 -0300

    realizando projeto pratico

--- Requisições/script.js
+++ Requisições/script.js
@@ -1,13 +1,1 @@
 // https://jsonplaceholder.typicode.com/posts
-
-function ciclou() {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      alert(`Seu Primeiro Post: ${json[0].title}`);
-    });
-}
-
-document.querySelector("#botao").addEventListener("click", ciclou);
